585f5cdd1b91793c45efd0b3a13090386ffca10f
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-22 11:49:06 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -6,8 +6,13 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const toast = document.createElement("li");
+  toast.textContent = "I'm a toast message.";
+  toast.classList.add("toast-container__message");
+  toastContainer.append(toast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  clearButton.addEventListener("click", () => (toastContainer.innerHTML = ""));
 });
